15714a401458442a8e87c50c051b84de447d56e6
konstantinminster <konstantin@minster.biz>
2024-06-12 18:44:14 +0200

    some stylings and comments works as well

--- journal.js
+++ journal.js
@@ -3,45 +3,102 @@
 // add button to comment
 // store comment in local storage
 
+function generateMyJournal() {
+  const output = document.getElementById("output-journal");
+  const getMyJournal = JSON.parse(localStorage.getItem("movies")) || [];
+  console.log(getMyJournal);
+  getMyJournal.forEach((element) => {
+    const getMovieTitle = element.Name;
+    const getMovieImage = element.img_url;
+    const getMovieID = element.ID;
+    let getComment = element.comment;
+    console.log(getMovieID);
 
-(() => {
-    const getMyJournal = JSON.parse(localStorage.getItem('movies')) || [];
-    console.log(getMyJournal);
-    getMyJournal.forEach((element, event) => {
-        const getMovieTitle = element.Name;
-        const getMovieImage = element.img_url;
-        const output = document.getElementById("output-journal");
-        const createCard = document.createElement("ul");
-        createCard.classList = "p-5 bg-green-500";
-        const createTitle = document.createElement("li");
-        createTitle.className = "movie-title";
-        createTitle.textContent = getMovieTitle;
-        const createPoster = document.createElement("li");
-        const createPosterImg = document.createElement("img");
-        createPosterImg.src = getMovieImage;
-        createPoster.appendChild(createPosterImg);
-        createPoster.className = "movie-poster";
-        output.appendChild(createCard);
-        createCard.appendChild(createPoster);
-        createCard.appendChild(createTitle);
+    // MovieCard erstellen und styling
+    const movieCard = document.createElement("ul");
+    movieCard.classList = "p-5 border";
+    movieCard.id = getMovieID;
+    const createTitle = document.createElement("li");
+    createTitle.className = "movie-title";
+    createTitle.textContent = getMovieTitle;
+    const createPoster = document.createElement("li");
+    const createPosterImg = document.createElement("img");
+    createPosterImg.src = getMovieImage;
+    createPoster.appendChild(createPosterImg);
+    createPoster.className = "movie-poster relative";
+    output.appendChild(movieCard);
+    
 
-        // const removeButton = document.createElement("button");
-        //     removeButton.classList = "add-journal bg-black text-white";
-        //     removeButton.textContent = "Add a Comment"
-        //     createCard.appendChild(removeButton);
+    //öffne comment dialog
+    const addCommentDialogBtn = document.createElement("button");
+    addCommentDialogBtn.classList = "absolute p-2 bg-black bottom-2 right-2 "
+    addCommentDialogBtn.innerHTML = `<img src="https://img.icons8.com/?size=30&id=1ukEkhevqhKc&format=png&color=ffffff">`;
+    createPoster.append(addCommentDialogBtn)
+  
+    
+    movieCard.append(createPoster, createTitle);
+
+    //Kommentar schreiben
+
+    const commentWindow = document.createElement("div");
+    commentWindow.classList = "comment-window hidden";
+    commentWindow.innerHTML = `<textarea id="text-${getMovieID}" class="comment-textarea w-full h-20 border"></textarea>`;
+    movieCard.append(commentWindow);
+
+    addCommentDialogBtn.addEventListener("click", () => {
+      commentWindow.classList.remove("hidden");
+    });
+
+    // Kommentar Speichern
+    const saveCommentBtn = document.createElement("button");
+    
+    saveCommentBtn.textContent = "Kommentar Speichern";
+    commentWindow.append(saveCommentBtn);
+
+    saveCommentBtn.addEventListener("click", () => {
+      const getCommentText = document.getElementById(
+        `text-${getMovieID}`
+      ).value;
+
+      const entries = JSON.parse(localStorage.getItem("movies")) || [];
+      console.log("Ergebnis2" + entries);
+      const entryIdToUpdate = getMovieID;
+      const newComment = getCommentText;
+      console.log(entryIdToUpdate);
+
+      for (let i = 0; i < entries.length; i++) {
+        if (entries[i].ID === entryIdToUpdate) {
+          entries[i].comment = newComment;
+          break;
+        }
+      }
+
+      localStorage.setItem("movies", JSON.stringify(entries));
+      location.reload();
 
 
-            // removeButton.addEventListener("click", (event) => {
-            //     console.log("zu localstorage hinzugefügt");
-            //     localStorage.removeItem('myJournal', event);
-            //     const objects = JSON.parse(localStorage.getItem('myJournal')) || [];
-            //     objects.splice(event, 1); // Remove the item at the given index
-            //     localStorage.setItem('myJournal', JSON.stringify(objects));
-            //   })
+    });
+
+    // Kommentar anzeigen
+    if (getComment !== "") {
+      console.log(getComment);
+      const commentView = document.createElement("p");
+      const commentViewHeadline = document.createElement("h4");
+      commentViewHeadline.textContent = "Meine Notizen: ";
+      commentView.textContent = getComment;
+      movieCard.append(commentViewHeadline, commentView);
+    }
 
 
-});
-})();
+    // STYLING
+    let btnStyle = "bg-black text-white w-full p-2";
+    saveCommentBtn.classList = btnStyle;
 
 
+    
 
+
+  });
+}
+
+generateMyJournal();
